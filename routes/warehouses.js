0a42177b2c56b772efa5c4a7829d0c583c157807
willhoare <williamhoare@gmail.com>
2023-01-25 18:19:27 -0500

    added get request for warehouses

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -4,8 +4,28 @@ const fs = require("fs");
 const warehouses = require("../data/warehouses.json");
 
 // GET WAREHOUSES
-router.get("/", (req, res, next) => {
-  res.status(201).json();
+router.get("/", (req, res) => {
+  try {
+    const data = warehouses.map((detail) => {
+      return {
+        id: detail.id,
+        warehouse_name: detail.warehouse_name,
+        address: detail.address,
+        city: detail.city,
+        country: detail.country,
+        contact_name: detail.contact_name,
+        contact_phone: detail.contact_phone,
+        contact_email: detail.contact_email,
+      };
+    });
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(400).json({
+      error: true,
+      message: "Server is unable to be reached currently. Please try again. ",
+      specific: error,
+    });
+  }
 });
 
 //GET SINGLE WAREHOUSE
