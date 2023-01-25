eba5a3c5fda5f4df9448b8913966614aa2f80e1c
willhoare <williamhoare@gmail.com>
2023-01-24 19:07:37 -0500

    started working on warehouses get

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const warehouses = require("../data/warehouses.json");
 
 // GET WAREHOUSES
 router.get("/", (req, res, next) => {
   res.status(201).json();
+});
+
+router.get("/", (req, res) => {
+  const readWarehouses = fs.readFileSync("../data/warehouses.json");
+  const warehouses = JSON.parse(readWarehouses);
+  res.json(warehouses);
 });
 
 module.exports = router;
