e550dfb1139599ba24174c8a999649d7fc9a08cc
Han Kim <hanwkim88@gmail.com>
2023-01-24 23:00:12 -0500

    Add router for single warehouse GET request.

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -7,4 +7,15 @@ router.get("/", (req, res, next) => {
   res.status(201).json();
 });
 
+//GET SINGLE WAREHOUSE
+router.get("/:id", (req, res) => {
+  const warehouseList = JSON.parse(
+    fs.readFileSync("./data/warehouses.json")
+  );
+
+  res.json(
+    warehouseList.find((warehouse) => req.params.id === warehouse.id)
+  );
+});
+
 module.exports = router;
