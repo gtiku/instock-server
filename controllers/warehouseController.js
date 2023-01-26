fa30a8882de775d31e995a33b4f21450ab6b9438
itskroms <keith.aristy@gmail.com>
2023-01-26 10:49:41 -0500

    test post req res from server

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,2 +1,36 @@
 const knex = require("knex")(require("../knexfile"));
-const inventory = require("../data/warehouses.json");
+const warehouses = require('../data/warehouses.json');
+
+
+
+const index = (_req, res) => {
+    knex
+      .select("*")
+      .from('warehouses')
+      .then((warehouses) => {
+        res.json(warehouses);
+      })
+      .catch((error) => {
+        res.status(400).json({
+          error: true,
+          message: "Server cannot be reached",
+          specific: error,
+        });
+      });
+  };
+
+const addWarehouse = (req, res) =>  {
+    console.log(res.body);
+
+    knex("warehouses")
+        .insert(req.body)
+        .then((warehouse)=> {
+            const newWarehouseURL = `/api/v1/warehouses/${warehouse[0]}`;
+            res.status(201).location(newWarehouseURL).send(newWarehouseURL);
+          })
+        .catch((err) => res.status(400).send(`Error creating Warehouse: ${err}`))
+    
+}
+
+module.exports = { index, addWarehouse };
+
