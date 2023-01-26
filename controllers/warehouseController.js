2646e30d0fe8276f29af1184667a848dee7fa587
willhoare <williamhoare@gmail.com>
2023-01-25 19:59:07 -0500

    knex get request is working

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,2 +1,20 @@
 const knex = require("knex")(require("../knexfile"));
 const inventory = require("../data/warehouses.json");
+
+const index = (_req, res) => {
+  knex
+    .select("*")
+    .from("warehouses")
+    .then((warehouses) => {
+      res.json(warehouses);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: true,
+        message: "Server cannot be reached",
+        specific: error,
+      });
+    });
+};
+
+module.exports = { index };
--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,32 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-const warehouses = require("../data/warehouses.json");
+// const warehouses = require("../data/warehouses.json");
+const warehouseController = require("../controllers/warehouseController");
 
-// GET WAREHOUSES
-router.get("/", (req, res) => {
-  try {
-    const data = warehouses.map((detail) => {
-      return {
-        id: detail.id,
-        warehouse_name: detail.warehouse_name,
-        address: detail.address,
-        city: detail.city,
-        country: detail.country,
-        contact_name: detail.contact_name,
-        contact_phone: detail.contact_phone,
-        contact_email: detail.contact_email,
-      };
-    });
-    res.status(201).json(data);
-  } catch (error) {
-    res.status(400).json({
-      error: true,
-      message: "Server is unable to be reached currently. Please try again. ",
-      specific: error,
-    });
-  }
-});
+// GET WAREHOUSES (KNEX)
+router.route("/").get(warehouseController.index);
 
 //GET SINGLE WAREHOUSE
 router.get("/:id", (req, res) => {
