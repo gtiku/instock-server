d5a507fbec97857467d2f2b305487a41be28446f
jessemcc <jesse.mcconnell@live.ca>
2023-01-25 19:59:36 -0500

    added get request with knex

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,2 +1,19 @@
 const knex = require("knex")(require("../knexfile"));
-const inventory = require("../data/inventories.json");
+
+const index = (_req, res) => {
+  knex
+    .select("*")
+    .from("inventories")
+    .then((inventories) => {
+      res.json(inventories);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: true,
+        message: "Server is unable to be reached currently. Please try again. ",
+        specific: error,
+      });
+    });
+};
+
+module.exports = { index };
