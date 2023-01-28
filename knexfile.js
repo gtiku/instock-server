e570d8d5ee573901e807ac19e46524252220a839
Han Kim <hanwkim88@gmail.com>
2023-01-28 14:30:17 -0500

    Remove duplicate routes.

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 // Update with your config settings.
 
 /**
- * @type { Object.<string, import("knex").Knex.Config> }
+ * 
  */
 module.exports = {
   client: "mysql",
--- routes/warehouses.js
+++ routes/warehouses.js
@@ -3,30 +3,18 @@ const router = express.Router();
 const fs = require("fs");
 const warehouseController = require("../controllers/warehouseController");
 
-// GET ALL WAREHOUSES
-// GET WAREHOUSES
-router.route("/").get(warehouseController.index);
-
-//GET SINGLE WAREHOUSE
-router.get("/:id", warehouseController.getWarehouse);
-
-//DELETE SINGLE WAREHOUSE
-router.delete("/:id", warehouseController.deleteWarehouse);
-
-//DELETE SINGLE WAREHOUSE
-router.delete("/:id", warehouseController.deleteWarehouse);
-
-//ADD WAREHOUSE TO WAREHOUSES
-router
-  .route("/")
-  .get(warehouseController.index)
-  .post(warehouseController.addWarehouse);
 router
   .route("/")
   .get(warehouseController.index)
   .post(warehouseController.addWarehouse);
 
-//GET INVENTORY BY WAREHOUSE ID
-router.route("/:id/inventories").get(warehouseController.getWarehouseInventory);
+router
+  .route("/:id")
+  .get(warehouseController.getWarehouse)
+  .delete(warehouseController.deleteWarehouse);
+
+router
+  .route("/:id/inventories")
+  .get(warehouseController.getWarehouseInventory);
 
 module.exports = router;
